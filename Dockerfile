a0bf095ca18d1980a426b0e3dcf39b6995d6705e
Wi9 <v9nineix@gmail.com>
2022-10-22 17:32:30 +0700

    [Add] install apollo grarpQL

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM node:16
+FROM node:14.15.1 as build
 
 WORKDIR /app
-COPY package*.json /app/
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package.json /app/package.json
 RUN npm install -g nodemon
-RUN npm install
 COPY . .
+RUN npm install
 CMD ["npm", "run", "start"]
--- src/index.js
+++ src/index.js
@@ -6,6 +6,41 @@ import route from "./routers";
 import bodyParser from "body-parser";
 const path = require('path')
 import cors from "cors"
+import { ApolloServer  , gql } from "apollo-server-express";
+// import { graphqlHTTP } from 'express-graphql'
+// import { buildSchema } from 'graphql'
+
+
+const grapQLServer = new ApolloServer({
+    playground: true,
+    typeDefs: gql`
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Hello world!',
+      },
+    }
+  })
+
+
+// Construct a schema, using GraphQL schema language
+// const schema = buildSchema(`
+// type Query {
+//   hello: String
+// }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// const root = {
+//     hello: () => {
+//       return 'Hello world!';
+//     },
+// };
+  
+
 
 
 const { server, database } = config;
@@ -13,19 +48,37 @@ mongoose.connect(database.uri, database.options);
 
 const app = express();
 
+
+
+
+// app.use('/graphql', graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true,
+//   }));
+
+
+grapQLServer.applyMiddleware({ app });
 app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 log(app);
 route(app);
+
+
 
 app.use('/uploads', express.static('uploads'));
 //app.use(express.static('folder'))
 app.use('/folder', express.static(path.join(__dirname, 'folder')))
 
+
+
 app.listen(server.port, server.host, () =>
-  console.log(`Server has started on ${server.host}:${server.port}`)
+  console.log(`Server has started on ${server.host}:${server.port} ${grapQLServer.graphqlPath}`)
 );
+
+
 
 export default app;

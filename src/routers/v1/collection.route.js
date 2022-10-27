d8198cae2e4fc3e5d933a1dd5294b887046a1a57
Jutaporn Kampeera-in <jutapornka@playwork.co.th>
2022-10-27 11:26:55 +0700

    Add function get collection by ownerId

--- src/routers/v1/collection.route.js
+++ src/routers/v1/collection.route.js
@@ -105,5 +105,35 @@ router.route("/uploadImage").post(upload.array('profile-files', 12), handle(cont
 
 
 
+// ========= find by ownerId ========= //
+/**
+ * @swagger
+ * /v1/collection/{ownerId}:
+ *   get:
+ *     tags: [Collection]
+ *     summary: get by ownerId
+ *     parameters:
+ *       - in: path
+ *         name: ownerId
+ *         description: ownerID
+ *         schema:
+ *           type: string
+ *           example: 5df206f819f1802f7e158f73
+ *     responses:
+ *       200:
+ *         description: Return get collection by ownerId
+ *         schema:
+ *           type: object
+ *           properties:
+ *             data:
+ *               $ref: '#/definitions/User'
+ *             version:
+ *               type: string
+ *               example: 1.0.0
+ */
+router.route("/:ownerId").get(handle(controller.findByOwnerId));
+
+
+
 
 export default router;

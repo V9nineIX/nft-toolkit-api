9d287b5582af40f4c68d574a95cd0b30986a7a58
Bussakorn Saetiew <bussakornsa@playwork.co.th>
2022-11-07 14:41:01 +0700

    add connect socket api

--- src/queues/generate-image-cosumer.js
+++ src/queues/generate-image-cosumer.js
@@ -1,52 +1,52 @@
 import { startCreating } from '../libs/genarate'
 import fsx from 'fs-extra';
 
-const generateImageProcess = async (job ,done) => {
+const generateImageProcess = async (job, done) => {
 
   // TODO Set parameter
 
-  const { layerConfigurations=null  , 
-    projectDir=null,
-    id=null,
-    ownerId=null
+  const { layerConfigurations = null,
+    projectDir = null,
+    id = null,
+    ownerId = null
   } = job.data
 
   let returnData = {
     id,
     ownerId,
-    "status":"completed"
- }
+    "status": "Completed"
+  }
 
   try {
 
 
-      let buildFolder = null
-      let jsonFolder  = null
+    let buildFolder = null
+    let jsonFolder = null
 
-      if(projectDir) {
-         buildFolder = `${projectDir}/build/image`
-         jsonFolder = `${projectDir}/build/json`
-        await fsx.ensureDir(buildFolder);
-        await fsx.ensureDir(jsonFolder);
-      }
+    if (projectDir) {
+      buildFolder = `${projectDir}/build/image`
+      jsonFolder = `${projectDir}/build/json`
+      await fsx.ensureDir(buildFolder);
+      await fsx.ensureDir(jsonFolder);
+    }
 
 
     const res = await startCreating({
-        layerConfigurations,
-        projectDir,
-        buildFolder,
-        jsonFolder,
-        job
-      })
+      layerConfigurations,
+      projectDir,
+      buildFolder,
+      jsonFolder,
+      job
+    })
 
- 
-      
-    done(null , { "result": returnData})
 
-  }catch(ex){
-      console.log(ex)
-      returnData.status = "fail"
-     done(new Error("can not genImage") , { "result":  returnData})
+
+    done(null, { "result": returnData })
+
+  } catch (ex) {
+    console.log(ex)
+    returnData.status = "fail"
+    done(new Error("can not genImage"), { "result": returnData })
   }
 
 

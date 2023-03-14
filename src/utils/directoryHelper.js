5c90e2d9ae4d615b60c66c2c7252e458f0971e51
Bussakorn Saetiew <bussakornsa@playwork.co.th>
2023-03-14 11:30:19 +0700

    add merge attributes and refresh opensea

--- src/utils/directoryHelper.js
+++ src/utils/directoryHelper.js
@@ -1,34 +1,38 @@
 import fs from 'fs'
-import fsextra  from 'fs-extra'
-import {  COLECTION_ROOT_FOLDER } from "../constants"
+import fsextra from 'fs-extra'
+import { COLECTION_ROOT_FOLDER } from "../constants"
 
 const getJsonDir = (projectDir) => {
     return `./${COLECTION_ROOT_FOLDER}/${projectDir}/build/json`
+}
+
+const getJsonFullDir = (projectDir) => {
+    return `${process.cwd()}/${COLECTION_ROOT_FOLDER}/${projectDir}/build/json`
 }
 
 const getImageDir = (projectDir) => {
     return `${process.cwd()}/${COLECTION_ROOT_FOLDER}/${projectDir}/build/image`
 }
 
-const getProjecrDir = (projectDir) => {
+const getProjectDir = (projectDir) => {
     return `${process.cwd()}/${COLECTION_ROOT_FOLDER}/${projectDir}`
 }
 
-const createDirectory =  async(directory) => {
-    return new Promise( async (resolve ,reject) => {  
-        try{
+const createDirectory = async (directory) => {
+    return new Promise(async (resolve, reject) => {
+        try {
 
             if (!fs.existsSync(directory)) {
-                 fs.mkdirSync(directory ,{ recursive: true })
-               resolve(true)
-             }
-             else{
+                fs.mkdirSync(directory, { recursive: true })
                 resolve(true)
-             }
-        
+            }
+            else {
+                resolve(true)
+            }
 
 
-        }catch(ex){
+
+        } catch (ex) {
             console.log(ex)
             reject(new Error("Can not create directory"))
         }
@@ -36,17 +40,17 @@ const createDirectory =  async(directory) => {
     })
 }
 
-const copyDirectory =  async(sourceFolder ,destinationFolder) => {
-    return new Promise( async (resolve ,reject) => {  
-       console.log(" destinationFolder", destinationFolder)
-           
+const copyDirectory = async (sourceFolder, destinationFolder) => {
+    return new Promise(async (resolve, reject) => {
+        console.log(" destinationFolder", destinationFolder)
+
         fsextra.copy(sourceFolder, destinationFolder, (err) => {
             if (err) {
-            console.error(err);
-            reject(err)
+                console.error(err);
+                reject(err)
             } else {
-            resolve(true)
-            console.log(`Successfully copied ${sourceFolder} to ${destinationFolder}`);
+                resolve(true)
+                console.log(`Successfully copied ${sourceFolder} to ${destinationFolder}`);
             }
         });
     })
@@ -55,9 +59,10 @@ const copyDirectory =  async(sourceFolder ,destinationFolder) => {
 
 
 module.exports = {
-    getJsonDir, 
+    getJsonDir,
+    getJsonFullDir,
     createDirectory,
     copyDirectory,
     getImageDir,
-    getProjecrDir  
+    getProjectDir
 }
